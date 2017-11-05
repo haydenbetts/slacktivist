637cfe3b7429bd00977f814bf827eedd0ca65d9d
haydenbetts <haydenbetts@gmail.com>
2017-11-05 08:15:38 -0800

    some level of player matching working...

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,12 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    # declaring an identification means that a attr_accessor is automatically
+    # set for that key. So we will be able to access Connection.uuid for every
+    # user. I think...
+    identified_by :uuid
+
+    def connect
+      self.uuid = SecureRandom.urlsafe_base64
+    end
   end
 end
